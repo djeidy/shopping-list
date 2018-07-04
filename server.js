086e8ea1807d4c142eef731570ee4dd72a04bffb
Mahamadou Djeidy Ba <mahamadouba@gmail.com>
2018-07-03 17:10:04 -0700

    add deployment configuration

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const bodyparser = require('body-parser'); 
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const path = require('path');
 
-const items = require('./routes/api/items'); 
+const items = require('./routes/api/items');
 
-const app = express(); 
+const app = express();
 
 //Bodyparser middlewares; 
-app.use(bodyparser.json()); 
+app.use(bodyparser.json());
 
 //DB config 
 const db = require('./config/keys').mongoURI
@@ -15,10 +16,18 @@ const db = require('./config/keys').mongoURI
 //Connect to mongo database
 mongoose.connect(db)
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err)); 
+    .catch(err => console.log(err));
 // Using Routes
-app.use('/api/items', items); 
+app.use('/api/items', items);
 
-const port = process.env.PORT || 5000; 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
